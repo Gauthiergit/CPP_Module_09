3d51df31d0217672cd3f31f8a7c2f9b8943ec7d7
Gauthier Peyre <gpeyre@c3r3p6.42perpignan.fr>
2024-07-03 17:53:50 +0200

    ex02

--- ex02/PmergeMe.hpp
+++ ex02/PmergeMe.hpp
@@ -6,7 +6,7 @@
 /*   By: gpeyre <gpeyre@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/02 11:53:50 by gpeyre            #+#    #+#             */
-/*   Updated: 2024/07/03 11:59:32 by gpeyre           ###   ########.fr       */
+/*   Updated: 2024/07/03 17:38:05 by gpeyre           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,27 @@
 #include <iostream>
 #include <vector>
 #include <list>
+#include <cstdlib>
+#include <cstring>
 #include <algorithm>
+#include <ctime>
 
+#define BLUE "\033[94m"
+#define RESET "\033[0m"
+#define RED "\033[31m"
+#define GREEN "\033[32m"
+#define YELLOW "\033[33m"
+
+
+class InvalidInput : public std::exception
+{
+	public:
+
+	virtual const char* what() const throw()
+	{
+		return ("Error.");
+	}
+};
 
 template< typename It >
 void merge(It result, It begin1, It end1, It begin2, It end2)
@@ -61,11 +80,18 @@ class PmergeMe
 
 	PmergeMe<Ct>(char **argv)
 	{
-		for (int i = 0; argv[i]; i++)
-			this->ct_input.push_back(argv[i]);
+		int nb = 0;
+		for (int i = 1; argv[i]; i++)
+		{
+			nb = atoi(argv[i]);
+			if (nb < 0)
+				throw InvalidInput();
+			else
+				this->ct_input.push_back(nb);
+		}
 	}
 
-	PmergeMe(const PmergeMe &copy){*this = copy}
+	PmergeMe(const PmergeMe &copy){*this = copy;}
 	~PmergeMe(){}
 
 	PmergeMe& operator=(const PmergeMe &change)
@@ -74,13 +100,13 @@ class PmergeMe
 		return (*this);
 	}
 
-	template< typename Ct >
+	
 	void fordJohnsonVecSort()
 	{
 		if (this->ct_input.size() <= 1) return;
 
 		std::vector<Ct> pairs;
-		Ct::iterator it = this->ct_input.begin();
+		typename Ct::iterator it = this->ct_input.begin();
 		while (it != this->ct_input.end())
 		{
 			Ct pair;
@@ -113,6 +139,63 @@ class PmergeMe
 			pairs = newPairs;
 		}
 		this->ct_input = pairs[0];
+	}
+
+	void fordJohnsonListSort()
+	{
+		if (this->ct_input.size() <= 1) return;
+
+		std::vector<Ct> pairs;
+		typename Ct::iterator it = this->ct_input.begin();
+		while (it != this->ct_input.end())
+		{
+			Ct pair;
+			pair.push_back(*it);
+			++it;
+			if (it != this->ct_input.end())
+			{
+				pair.push_back(*it);
+				++it;
+			}
+			pair.sort();
+			pairs.push_back(pair);
+		}
+
+		while (pairs.size() > 1)
+		{
+			std::vector<Ct> newPairs;
+			for (size_t i = 0; i < pairs.size(); i += 2)
+			{
+				if (i + 1 < pairs.size())
+				{
+					Ct merged;
+					merged.resize(pairs[i].size() + pairs[i + 1].size());
+					merge(merged.begin(), pairs[i].begin(), pairs[i].end(), pairs[i + 1].begin(), pairs[i + 1].end());
+					newPairs.push_back(merged);
+				}
+				else
+					newPairs.push_back(pairs[i]);
+			}
+			pairs = newPairs;
+		}
+		this->ct_input = pairs[0];
+	}
+
+	void displayRes()
+	{
+		if (this->ct_input.size() <= 10)
+		{
+			for (typename Ct::iterator it = this->ct_input.begin(); it != this->ct_input.end(); ++it)
+				std::cout << *it << " ";
+		}
+		else
+		{
+			typename Ct::iterator it = this->ct_input.begin();	
+			for (int i = 0; i < 4 && it != this->ct_input.end(); i++, ++it)
+				std::cout << *it << " ";
+			std::cout << "[...]";
+		}
+		std::cout << std::endl;
 	}
 };
 
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,36 +6,66 @@
 /*   By: gpeyre <gpeyre@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/03 10:31:50 by gpeyre            #+#    #+#             */
-/*   Updated: 2024/07/03 10:54:50 by gpeyre           ###   ########.fr       */
+/*   Updated: 2024/07/03 17:49:26 by gpeyre           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PmergeMe.hpp"
 
-// int main() {
+void isItDigit(int argc, char **argv)
+{
+	size_t j;
+	for (int i = 1; i < argc; i++)
+	{
+		j = 0;
+		for (; j < strlen(argv[i]); j++)
+		{
+			if (!isdigit(argv[i][j]))
+				throw InvalidInput();
+		}
+	}
+}
 
-//     std::vector<int> arr;
-// 	arr.push_back(38);
-// 	arr.push_back(27);
-// 	arr.push_back(43);
-// 	arr.push_back(3);
-// 	arr.push_back(9);
-// 	arr.push_back(82);
-// 	arr.push_back(10);
+int main(int argc, char **argv)
+{
+	if (argc == 1)
+	{
+		std::cerr << RED << "Use: " << argv[0] << " <nb> <nb> .." << RESET << std::endl;
+		return (1);
+	}
+	try
+	{
+		isItDigit(argc, argv);
+		std::cout << GREEN << "Vector" << RESET << std::endl;
+		PmergeMe< std::vector<int> > sortThat(argv);
+		std::cout << "Before : ";
+		sortThat.displayRes();
+		std::clock_t start = std::clock();
+		sortThat.fordJohnsonVecSort();
+		std::clock_t end = std::clock();
+		double elapsedTimeVec = static_cast<double>(end - start) / CLOCKS_PER_SEC;
+		std::cout << "After : ";
+		sortThat.displayRes();
+		std::cout << "Time to process of "<< argc - 1 << " elements with "<<
+			GREEN << "std::vector" << RESET << " : " << elapsedTimeVec << " us" << std::endl;
+		std::cout << "-----------------------------------------------" << std::endl;
+		std::cout << GREEN << "List" << RESET << std::endl;
+		PmergeMe< std::list<int> > sortThis(argv);
+		std::cout << "Before : ";
+		sortThis.displayRes();
+		start = std::clock();
+		sortThis.fordJohnsonListSort();
+		end = std::clock();
+		double elapsedTimeLst = static_cast<double>(end - start) / CLOCKS_PER_SEC;
+		std::cout << "After : ";
+		sortThis.displayRes();
+		std::cout << "Time to process of "<< argc - 1 << " elements with "<<
+			GREEN << "std::list" << RESET << " : " << elapsedTimeLst << " us" << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << RED << e.what() << RESET << '\n';
+	}
 
-//     std::cout << "Avant le tri: ";
-//     for (size_t i = 0; i < arr.size(); ++i) {
-//         std::cout << arr[i] << " ";
-//     }
-//     std::cout << std::endl;
-
-//     fordJohnsonSort(arr);
-
-//     std::cout << "Après le tri: ";
-//     for (size_t i = 0; i < arr.size(); ++i) {
-//         std::cout << arr[i] << " ";
-//     }
-//     std::cout << std::endl;
-
-//     return 0;
-// }
+	return 0;
+}
